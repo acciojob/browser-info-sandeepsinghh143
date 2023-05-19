9e983b4229206613a307f91836c106eb2ec2303d
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-19 13:50:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let browserName=navigator.appName;
+let version=navigator.appVersion;
+let browserInfo=document.getElementById("browser-info");
+browserInfo.innerText="You are using " + browserName + " version " + version;
